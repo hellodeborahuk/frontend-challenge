7d74f7a665b8f5a304ed4124ee8f78b6178b1293
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2024-04-25 16:18:45 +0100

    installed nuxt and tailwind. Started blog listing

---
+++ README.md
@@ -0,0 +1,11 @@
+# Frontend Challenege
+
+## Time estimates
+
+Blog listing - 1 hour
+
+- no hover state design
+- blog posts aren't exactly center but look like they should be (143px right and 152px left)
+- hard to tell pixels under 'blog' heading as text box is too big.
+- is 12px accessible?
+- I'd usually create a blogCard component and then loop over in blogListing but as this is static I've just put all 3 cards in blogListing.
---
+++ components/BlogListing.vue
@@ -0,0 +1,46 @@
+<template>
+    <h2 class="text-[27px] font-bold text-center my-[119px]">Blogs</h2>
+    <div class="grid grid-cols-3 gap-20 max-w-6xl mx-auto">
+        <div class="relative">
+            <div class="relative" :style="{background: `linear-gradient(90deg, rgba(123, 123, 123, 0.772057) 45.4%, rgba(157, 157, 157, 0.70918) 63.87%, rgba(217, 217, 217, 0) 86.53%), center center / cover no-repeat url(../blog1.png);`}">
+                <h3 class="absolute px-10 bottom-10 text-white text-lg font-bold">How to build a cash reserve in your MSP business</h3>
+            </div>
+            <div class="px-10 py-2 w-[291px] text-[13px] bg-brand-beta border-brand-beta rounded-r-full text-brand-charlie absolute transform -translate-y-1/2 border left-0">
+                <p>May 4, 2022</p>
+                <p>Written by Sundack</p>
+            </div>
+            <div class="bg-brand-delta text-sm p-10 space-y-5">
+                <p>Cash reserves refers to an amount of money on hand specifically intended to deal with emergencies. These are typically short-term emergencies, but as the last few years have taught us, a few weeks doesn’t always pan out how we think. Another use for reserve is…</p>
+                <p><a href="/" class="button">read more</a></p>
+            </div>
+        </div>
+        <div class="relative">
+            <div class="relative">
+                <image src="../blog2.png" alt="test image" loading="lazy" />
+                <h3 class="absolute px-10 bottom-10 text-white text-lg font-bold">How to build a cash reserve in your MSP business</h3>
+            </div>
+            <div class="px-10 py-2 w-[291px] text-[13px] bg-brand-beta border-brand-beta rounded-r-full text-brand-charlie absolute transform -translate-y-1/2 border left-0">
+                <p>May 4, 2022</p>
+                <p>Written by Sundack</p>
+            </div>
+            <div class="bg-brand-delta text-sm p-10 space-y-5">
+                <p>Cash reserves refers to an amount of money on hand specifically intended to deal with emergencies. These are typically short-term emergencies, but as the last few years have taught us, a few weeks doesn’t always pan out how we think. Another use for reserve is…</p>
+                <p><a href="/" class="button">read more</a></p>
+            </div>
+        </div>
+        <div class="relative">
+            <div class="relative">
+                <image src="../blog3.png" alt="test image" loading="lazy" />
+                <h3 class="absolute px-10 bottom-10 text-white text-lg font-bold">How to build a cash reserve in your MSP business</h3>
+            </div>
+            <div class="px-10 py-2 w-[291px] text-[13px] bg-brand-beta border-brand-beta rounded-r-full text-brand-charlie absolute transform -translate-y-1/2 border left-0">
+                <p>May 4, 2022</p>
+                <p>Written by Sundack</p>
+            </div>
+            <div class="bg-brand-delta text-sm p-10 space-y-5">
+                <p>Cash reserves refers to an amount of money on hand specifically intended to deal with emergencies. These are typically short-term emergencies, but as the last few years have taught us, a few weeks doesn’t always pan out how we think. Another use for reserve is…</p>
+                <p><a href="/" class="button">read more</a></p>
+            </div>
+        </div>
+    </div>
+</template>
